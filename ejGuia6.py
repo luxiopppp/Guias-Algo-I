7d37ada95ec202406db4eec6ecd7578dbee0730c
Tu Nombre <you@example.com>
2024-05-27 11:45:54 -0300

    a

--- ejGuia6.py
+++ ejGuia6.py
@@ -4,18 +4,6 @@ def imprimir_hola_mundo():
     print("hola mundo")
 
 
-def primes(n: int):
-    sieve = [True] * n
-
-    res = []
-
-    for i in range(2,n):
-        if sieve[i]:
-            res.append(i)
-            for j in range(i*i,n,i):
-                sieve[j] = False
-
-    return res
 
 
 
