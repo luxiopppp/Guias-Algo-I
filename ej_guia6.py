3c4579c10af00958a05d3d1d6faab5f9fd55f3ee
luxiopppp <lucio.ese@gmail.com>
2024-08-05 16:04:13 -0300

    a

---
+++ ej_guia6.py
@@ -0,0 +1,16 @@
+# Ej 1
+
+
+def imprimir_hola_mundo():
+    print("hola mundo")
+
+
+# Ej 2
+
+
+def imprimir_saludo(nombre: str):
+    print("Hola " + nombre)
+
+
+def raiz_cuadrada_de(numero: int):
+    return numero ** (0.5)
